e55c2b41dc6b32a98268b0fb7d89175251e1439b
BVasquez07 <bvasq07@gmail.com>
2025-11-08 16:17:46 -0500

    add: exception to config file and fixing some of the root main.py files

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #The code here will pull in the connections to the API and leverage the ETL modules in the src directory.
-from src.Extract.main import testing
+from src.main import compile_ETL_data
 from config import api_keys
 
 
-print(api_keys)
+def main() -> None:
+    print(api_keys['finnhub'])
+    etl_data = compile_ETL_data(api_keys["finnhub"], api_keys["yahoo_fin"])
+    print("ETL Data Compiled:", etl_data)
 
 
 
-
-
+if __name__ == "__main__":
+    main()
